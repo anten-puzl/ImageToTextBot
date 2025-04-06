a45da0c24d03fd1e07036e1537a17b9b26397c1a
Anton Pazulskyi <a.pazulskyi@brimit.com>
2025-04-06 16:35:06 +0200

    add requirements.txt

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import os
 from dotenv import load_dotenv
-from telegram.ext import Updater, MessageHandler, Filters
-#comment1
+from telegram.ext import ApplicationBuilder, MessageHandler, filters
+
 # Load environment variables from .env file
 load_dotenv()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
-def handle_message(update, context):
+async def handle_message(update, context):
     # Respond with "hello" to any text message
-    update.message.reply_text("hello")
+    await update.message.reply_text("hello2")
 
 def main():
     if not TELEGRAM_TOKEN:
@@ -16,16 +16,14 @@ def main():
         return
 
     # Initialize the bot with the token
-    updater = Updater(TELEGRAM_TOKEN, use_context=True)
-    dp = updater.dispatcher
+    application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
     # Handle all text messages (excluding commands)
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     # Start polling for updates
-    updater.start_polling()
+    application.run_polling()
     print("✅ Bot is running.")
-    updater.idle()
 
 if __name__ == '__main__':
     main()
